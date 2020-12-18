04859448ed7132c1e3446dc1dca5af8d44995190
Alexandre V <algator67@hotmail.fr>
2020-12-18 14:39:26 +0100

    Ajout de la librairie
    Les principales structures sont présentes avec les commentaires doxygen

--- structures.h
+++ structures.h
@@ -1,12 +1,45 @@
+#ifndef STRUCTURES_HEADER
+#define STRUCTURES_HEADER
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
+/**
+ * \struct liste
+ * \brief Liste chaînée de prémices : élément d'une règle
+ */
 typedef struct l{
-  char* premice;
-  struct l* suivant;
+  char* premice; /** \brief chaîne de caractères contenant l'intitulé d'une prémice */
+  struct l* suivant; /** \brief pointeur sur l'élément suivant de la liste */
 } liste;
 
+/**
+ * \struct regle
+ * \brief Produit cartésien d'une liste de prémices et d'une conclusion
+ */
 typedef struct{
-  list* premices;
-  char* conclusion;
+  liste* premices; /** \brief liste de premices */
+  char* conclusion; /** \brief chaîne de caractère contenant l'intitulé d'une conclusion */
 } regle;
+
+/**
+ * \struct connaissance
+ * \brief élément d'une base de connaissance : liste de regle
+ */
+typedef struct bc_elem{
+  regle* regle; /** \brief liste de regles */
+  struct bc_elem* suivant; /** \brief pointeur sur l'élément suivant de la base de connaissances */
+} connaissance;
+
+/**
+ * \struct BC
+ * \brief liste de connaissances
+ */
+typedef connaissance* BC;
+
+
+
+
+
+#endif
