fe0a19eda12f0321f14066dc713ad5b6305febc1
Alexandre V <algator67@hotmail.fr>
2021-01-03 18:26:20 +0100

    Ajout commentaires
    J'ai ajouté les commentaires sur toutes les fonctions.

--- fonction.h
+++ fonction.h
@@ -11,16 +11,52 @@
 
 /* Fonctions */
 
+/**
+ * \fn void Ajout_premisse(regle r, char* s)
+ * \brief fonction permettant d'ajouter une prémisse dans une règle
+ * \param r - regle que l'on souhaite altérer
+ * \param s - chaîne de caractère / intitulé de la prémisse que l'on veut ajouter
+ */
 void Ajout_premisse(regle r, char* s);
 
+/**
+ * \fn bool Si_premisse(liste* pp, char* s)
+ * \brief Fonction permettant de savoir si une prémisse appartient à une règle
+ * \param pp - une liste de prémisse que l'on va scanner
+ * \param s - une chaîne de caractères correspondant à la prémisse que l'on recherche dans pp
+ * \return un booléen : FAUX : s n'appartient pas à pp, VRAI : s appartient à pp
+ */
 bool Si_premisse(liste* pp, char* s);
 
+/**
+ * \fn bool Pas_premisse(regle r)
+ * \brief fonction permettant de savoir si une regle possède une prémisse
+ * \param r - regle dont on cherche à savoir si elle possède au moins une prémisse
+ * \return un booléen : VRAI : r n'a pas de prémisses, FAUX : r a au moins une prémisse
+ */
 bool Pas_premisse(regle r);
 
+/**
+ * \fn char* Conclusion_regle(regle r)
+ * \brief fonction permettant d'obtenir la conclusion d'une règle
+ * \param r - regle dont on veut connaitre la conclusion
+ * \return une chaîne de caractères correspondant à l'intitulé de la conclusion de la règle
+ */
 char* Conclusion_regle(regle r);
 
+/**
+ * \fn BC Creer_base()
+ * \brief Fonction permettant de créer une base vide
+ * \return une base e connaissances vide
+ */
 BC Creer_base();
 
+/**
+ * \fn regle Regle_tete_base(BC b)
+ * \brief Fonction permettant de connaitre la règle de tête d'une base
+ * \param b - une base de connaissances
+ * \return une regle, la regle de tête de b
+ */
 regle Regle_tete_base(BC b);
 
 /**
@@ -62,15 +98,17 @@ regle inserer_conclusion(regle r, char* c_conclusion);
  */
 regle supprimer_premisse(regle r,char* intitule_premisse);
 
-/** \fn liste* premisse_tete(regle r)
- * \brief fonction permettant de donner de récupérer la prémisse de tête dans une regle
+/** 
+ * \fn liste* premisse_tete(regle r)
+ * \brief fonction permettant de récupérer la prémisse de tête dans une regle
  * \param r - regle dont on veut connaitre la prémisse de tête
  * \return une premisse - la prémisse de tête
  */
 liste* premisse_tete(regle r);
 
 
-/** \fn BC ajouter_regle(BC base, regle r)
+/** 
+ * \fn BC ajouter_regle(BC base, regle r)
  * \brief fonction permettant de rajouter une regle r dans une base de connaissances
  * \param base - Base de connaissance que l'on va altérer
  * \param r - regle que l'on veut ajouter à notre base de connaissances
